e6b8988a94dc5a1618492b42dd369f5f1674f827
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2023-07-21 11:11:59 +0100

    added files

--- uploadtosource.sh
+++ uploadtosource.sh
@@ -1,15 +1,15 @@
 rm -rf upload_dir
 mkdir upload_dir
 cd upload_dir
+git clone git@github.com:CentralLaserFacility/source-downloads.git -b master
 git clone git@github.com:CentralLaserFacility/motor.git -b development_clf --recurse-submodules motor-R7-2-2
 
 tar -czvf motor-R7-2-2.tar.gz motor-R7-2-2/
 
 rm -rf motor-R7-2-2
 
-git clone https://github.com/CentralLaserFacility/source-downloads -b master
 
-mv source-downloads/EPICS/test source-downloads/EPICS/test_"`date +"%d-%m-%Y"`"
+mv source-downloads/EPICS/motor-R7-2-2.tar.gz source-downloads/EPICS/test_"`date +"%d-%m-%Y"`"
 mv motor-R7-2-2.tar.gz source-downloads/EPICS/
 
 cd source-downloads/
